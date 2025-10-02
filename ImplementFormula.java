8067112047d9149075785b806e194ab42b23676b
Deecah <pchanhdn@gmail.com>
2025-10-02 14:23:42 +0700

    add case multiply

--- ImplementFormula.java
+++ ImplementFormula.java
@@ -1,5 +1,7 @@
 private double calculateFormula(SIGN sign, double arg1, double arg2) {
     switch (sign) {
+        case MULTIPLY:
+            return arg1 * arg2;
         default:
             break;
     }
